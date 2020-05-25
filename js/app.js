d477ae3f95ac2e42b2c84c47bd983e850bdd5f4f
Michael Neely <michael@michaelneely.net>
2020-05-24 23:16:35 -0400

    Added the reading values from the form

--- js/app.js
+++ js/app.js
@@ -4,3 +4,24 @@ const cryptoAPI = new CryptoAPI();
 const ui = new UI();
 
 
+const form = document.getElementById('form');
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+        // read currency
+        const currencySelect = document.getElementById('currency').value;
+
+        // read cryptocurrency
+        const cryptoCurrencySelect = document.getElementById('cryptocurrency').value;
+
+        // console.log(currencySelect + ': ' +cryptoCurrencySelect);
+        if(currencySelect === '' || cryptoCurrencySelect === '') {
+            ui.printCryptoCurrenciesMessage('All the fields are mandatory', 'deep-orange darken-4 card-panel');
+            console.log('error');
+        } else {
+            console.log('success');
+        }
+});
